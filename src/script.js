102ae4497a5f6586aa4a9ff6d597de886fcb274a
Kendi Hashimoto <kendi.campos@hotmail.com>
2022-10-06 20:23:53 -0300

    light mode

--- src/script.js
+++ src/script.js
@@ -1,18 +1,35 @@
 toDoList = () => {
+   // FUNCTIONALITIES
    const inputTask = document.getElementById('new-todo-input');
    const addButton = document.querySelector('.check-mark');
    const tasks = document.querySelector('.tasks');
-   const darkLightMode = document.querySelector('.dark-light-icon');
 
    // CREATE TASK
-   createTask = (event) => {
-      event.preventDefault();
-      let text = inputTask;
-   };
+
    
    // MARK TASK
 
 
    // DELETE TASK
+
+   // THEMES
+   const body = document.querySelector('body');
+
+   // LIGHT THEME
+   const lightIcon = document.querySelector('.sun-icon');
+
+   lightIcon.addEventListener("click", () => {
+      body.classList.remove('dark');
+      body.classList.add('light');      
+   });
+
+   // DARK THEME
+   const darkIcon = document.querySelector('.moon-icon'); 
+
+   darkIcon.addEventListener("click", () => {
+      body.classList.remove('light');
+      body.classList.add('dark');
+   });
+
 }
 toDoList();
