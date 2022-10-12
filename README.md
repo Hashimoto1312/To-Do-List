cbd0ad72e0774f8907638852b79267a1a7e73621
Kendi Hashimoto <kendi.campos@hotmail.com>
2022-10-11 21:08:02 -0300

    mark tasks ok

--- README.md
+++ README.md
@@ -8,6 +8,7 @@
   - [Table of contents](#table-of-contents)
   - [Overview](#overview)
     - [The challenge](#the-challenge)
+    - [Upgrades for future versions](#upgrades-for-future-versions)
     - [Screenshot](#screenshot)
     - [Links](#links)
   - [My process](#my-process)
@@ -27,12 +28,17 @@ Users should be able to:
 - [x] View the optimal layout for the app depending on their device's screen size
 - [] See hover states for all interactive elements on the page
 - [] **Add** new todos to the list
-- [] **Mark** todos as complete
+- [x] **Mark** todos as complete
 - [] **Delete** todos from the list
 - [] **Filter** by all/active/complete todos
 - [] **Clear** all completed todos
-- [] **Toggle** light and dark mode
+- [x] **Toggle** light and dark mode
 - [] **Bonus**: Drag and drop to reorder items on the list
+
+### Upgrades for future versions
+
+- [] Remove the check-mark hover when clicking on the task circle
+- [] Mark off task
 
 ### Screenshot
 
--- src/script.js
+++ src/script.js
@@ -1,35 +1,56 @@
 toDoList = () => {
-   // FUNCTIONALITIES
-   const inputTask = document.getElementById('new-todo-input');
-   const addButton = document.querySelector('.check-mark');
-   const tasks = document.querySelector('.tasks');
+   const body = document.querySelector('body');
+   const inputTask = document.querySelector('new-todo-input');
+   const checkIcon = document.querySelector('check-icon');
+   
+   // ADD TASK
+   const addBtn = document.querySelector('.check-mark');
+   addBtn.addEventListener("click", () => {
 
-   // CREATE TASK
+   });
+   
+   // DELETE TASK
+   const deleteBtn = document.querySelector('.delete-button');
+   deleteBtn.addEventListener("click", () => {
+
+   });
+
+   // MARK AND MARK OFF TASK
+   markTask = () => {
+      checkTask.classList.add('check-task-background')
+      body.classList.add('completed-task');
+   }
+
+   const checkTask = document.querySelector('.todo-item .check-mark');
+   checkTask.addEventListener('click', () => {
+      if (!checkTask.classList.contains('.check-task-background')) {
+         markTask();
+      }
+   });
+
+   // FILTER
 
    
-   // MARK TASK
+   // CLEAR COMPLETED
 
+   const reoderTask = document.querySelector('.todo-item');
+   // REORDER
 
-   // DELETE TASK
-
+   
    // THEMES
-   const body = document.querySelector('body');
 
    // LIGHT THEME
    const lightIcon = document.querySelector('.sun-icon');
-
    lightIcon.addEventListener("click", () => {
       body.classList.remove('dark');
       body.classList.add('light');      
    });
-
+   
    // DARK THEME
    const darkIcon = document.querySelector('.moon-icon'); 
-
    darkIcon.addEventListener("click", () => {
       body.classList.remove('light');
       body.classList.add('dark');
-   });
-
-}
+   })
+}   
 toDoList();
